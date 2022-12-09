79fb650b0167b14d1e5394e932a3557fd5eba875
vetsinen <vetsinen@gmail.com>
2022-12-10 00:21:00 +0200

    features added

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+import repository #import save_location,get_all_locations, get_last_locations
+
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from typing import Optional
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+class Location(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    title: str
+    review: str
+    long: float
+    lat: float
 
 class Msg(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
     msg: str
 
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
 @app.get("/")
-async def root():
-    return {"message": "Hello railway openswarm2"}
+async def index():
+    return FileResponse('static/add-location.html')
 
+@app.get("/locations")
+async def map():
+    return FileResponse('static/locations.html')
+
+@app.get("/map")
+async def map():
+    return FileResponse('static/map.html')
+
+@app.post("/api")
+async def location(location: Location):
+    print(location)
+    id = repository.save_location(location)
+    return {"status": 200}
+
+@app.get("/api/all")
+async def location():
+    return repository.get_all_locations()
+
+@app.get("/api/last")
+async def last_location():
+    return repository.get_last_locations()
+
+@app.delete("/api/{id}")
+async def delete(id:str):
+    repository.remove(id)
+    return {"v":id}
 
 @app.get("/path")
 async def demo_get():
---
+++ repository.py
@@ -0,0 +1,70 @@
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+from bson.json_util import dumps
+from bson.json_util import loads
+from bson.objectid import ObjectId
+
+load_dotenv()
+client = MongoClient('mongodb://mongo:AvK7b6FjmSG1iSFUTQ3S@containers-us-west-73.railway.app:6831')
+db = client.test
+collection = db.locations
+
+# {
+#     name: "Central Park",
+#     location: {type: "Point", coordinates: [-73.97, 40.77]},
+#     category: "Parks"
+# },
+def save_location(location):
+    id = collection.insert_one({
+        "title": location.title,
+        "location": {
+            "type": "Point",
+            "coordinates":[location.long,  location.lat]
+        },
+        "review": location.review,
+        "creation": datetime.now(),
+    }).inserted_id
+    return id
+
+def get_last_locations():
+    #TODO: use pydantic serializer
+    cursor = collection.find({}, {'_id': 1, 'title':1, 'review':1})\
+        .sort([("creation", pymongo.DESCENDING)]).limit(3)
+    docs =  loads(dumps(cursor))
+
+    rez = []
+    for loc in docs:
+        print(type(loc), str(loc['_id']))
+        rez.append({
+            "id": str(loc['_id']),
+            "title": loc['title'],
+            "review": loc['review'][0:15]
+        })
+    return rez
+
+def get_all_locations():
+    # TODO: use pydantic serializer
+    cursor = collection.find({}, {'_id': 1, 'title': 1, 'review': 1})
+    docs = loads(dumps(cursor))
+
+    rez = []
+    for loc in docs:
+        rez.append({
+            "id": str(loc['_id']),
+            "title": loc['title'],
+            "review": loc['review']
+        })
+    return rez
+
+def remove(id:str):
+    print(type(id), id)
+    collection.delete_one({'_id': ObjectId(id)})
+
+if __name__ == '__main__':
+    db = client.test
+    collection = db.coll1
+    post_id = collection.insert_one({"value":41}).inserted_id
+    print(post_id)
